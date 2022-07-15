00bd447d6491706c537b8fb7fc3423100fb90e96
alexsalvo1 <alexsalvo1@gmail.com>
2022-07-15 20:29:39 +0200

    fix

--- createCfKonEKS.sh
+++ createCfKonEKS.sh
@@ -9,8 +9,7 @@ cd ../"2-EKSCluster"
 source ./createEKSCluster.sh
 
 cd ../"3-CfK"
-if $1 = "advanced"
-then
+if [[ $1 != "advanced" ]]; then
     source ./createCfK.sh
 else
     source ./createCfK_adv.sh

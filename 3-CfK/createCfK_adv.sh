958f374f7bdc8948b814a9482467fc74eb3f7aa8
alexsalvo1 <alexsalvo1@gmail.com>
2022-07-20 00:04:51 +0200

    adding ssl encryption and external access on the advanced cluster

--- 3-CfK/createCfK_adv.sh
+++ 3-CfK/createCfK_adv.sh
@@ -3,7 +3,7 @@
 kubectl create namespace $namespace
 kubectl config set-context --current --namespace $namespace
 
-mkdir -p $cfk_dir
+#mkdir -p $cfk_dir
 #helm pull confluentinc/confluent-for-kubernetes \
 #  --untar --untardir=$cfk_dir
 #kubectl apply -f $cfk_dir/confluent-for-kubernetes/crds
@@ -12,22 +12,49 @@ mkdir -p $cfk_dir
 helm repo add confluentinc https://packages.confluent.io/helm
 helm repo update
 helm upgrade --install confluent-operator confluentinc/confluent-for-kubernetes --namespace $namespace
-kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=60s
-kubectl apply -f ./resources/zookeeper.yaml
-kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=60s
-kubectl apply -f ./resources/broker.yaml
-kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=90s
-kubectl apply -f ./resources/schema_registry.yaml
-kubectl apply -f ./resources/connect.yaml
-kubectl apply -f ./resources/ksqldb.yaml
-kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=120s
-kubectl apply -f ./resources/confluent_control_center.yaml
-kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=60s
-kubectl apply -f ./resources/rest_proxy.yaml
+
+mkdir -p sslcerts
+openssl genrsa -out sslcerts/ca-key.pem 2048
+
+openssl req -new -key sslcerts/ca-key.pem -x509 \
+  -days 1000 \
+  -out sslcerts/ca.pem \
+  -subj "/C=US/ST=CA/L=MountainView/O=Confluent/OU=Operator/CN=TestCA"
+
+kubectl create secret tls ca-pair-sslcerts \
+  --cert=sslcerts/ca.pem \
+  --key=sslcerts/ca-key.pem -n $namespace
+
+#kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=60s
+#kubectl apply -f ./resources/zookeeper.yaml
+#kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=60s
+#kubectl apply -f ./resources/broker.yaml
+#kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=90s
+#kubectl apply -f ./resources/schema_registry.yaml
+#kubectl apply -f ./resources/connect.yaml
+#kubectl apply -f ./resources/ksqldb.yaml
+#kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=120s
+#kubectl apply -f ./resources/confluent_control_center.yaml
+#kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=60s
+#kubectl apply -f ./resources/rest_proxy.yaml
+
+kubectl apply -f ./resources/confluent-platform-tls-only-external-listener.yaml
 kubectl wait --for=condition=ready pods --all --namespace $namespace --timeout=30s
-kubectl apply -f https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/producer-app-data.yaml
-kubectl wait --for=condition=ready pod/elastic-0 --namespace $namespace --timeout=60s
 kubectl get pods
+
+cat <<EOF > ./resources/kafka.properties
+bootstrap.servers=kafka.confluent.svc.cluster.local:9071
+security.protocol=SSL
+ssl.truststore.location=/mnt/sslcerts/truststore.jks
+ssl.truststore.password=mystorepassword
+EOF
+
+kubectl create secret generic kafka-client-config-secure \
+  --from-file=./resources/kafka.properties -n $namespace
+
+kubectl apply -f ./resources/secure-producer-app-data.yaml -n $namespace
+
+#kafka-console-producer    --topic test    --bootstrap-server kafka.confluent.svc.cluster.local:9092    --property parse.key=true    --property key.separator=,    --producer.config ./producer.config
 
 #curl -O https://confluent-for-kubernetes.s3-us-west-1.amazonaws.com/confluent-for-kubernetes-2.4.0.tar.gz
 #tar -xvf confluent-for-kubernetes-2.4.0.tar.gz \
--- 3-CfK/deleteCfK_adv.sh
+++ 3-CfK/deleteCfK_adv.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
-kubectl delete -f https://raw.githubusercontent.com/confluentinc/confluent-kubernetes-examples/master/quickstart-deploy/producer-app-data.yaml
+kubectl delete -f ./resources/secure-producer-app-data.yaml -n $namespace
 kubectl wait --for=delete pod/elastic-0 --timeout=60s
 
-kubectl delete -f ./resources/zookeeper.yaml
-kubectl delete -f ./resources/broker.yaml
-kubectl delete -f ./resources/schema_registry.yaml
-kubectl delete -f ./resources/connect.yaml
-kubectl delete -f ./resources/ksqldb.yaml
-kubectl delete -f ./resources/confluent_control_center.yaml
-kubectl delete -f ./resources/rest_proxy.yaml
-kubectl wait --for=delete pods --all --namespace $confluent --timeout=60s
-#kubectl delete -f ./resources/cfk-permissions.yaml
-#kubectl delete -f $cfk_dir/confluent-for-kubernetes/crds
-kubectl delete namespace $namespace
+#kubectl delete -f ./resources/zookeeper.yaml
+#kubectl delete -f ./resources/broker.yaml
+#kubectl delete -f ./resources/schema_registry.yaml
+#kubectl delete -f ./resources/connect.yaml
+#kubectl delete -f ./resources/ksqldb.yaml
+#kubectl delete -f ./resources/confluent_control_center.yaml
+#kubectl delete -f ./resources/rest_proxy.yaml
+#kubectl wait --for=delete pods --all --namespace $confluent --timeout=60s
 
-rm -R cfk-dir
-rm -R confluent-for-kubernetes
+kubectl delete -f ./resources/confluent-platform-tls-only-external-listener.yaml
+
+kubectl delete secret kafka-client-config-secure -n $namespace
+kubectl delete secret ca-pair-sslcerts -n $namespace
+#helm delete operator -n $namespace
+
+kubectl delete namespace $namespace
+#(
+#NAMESPACE=your-rogue-namespace
+#kubectl proxy &
+#kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
+#curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+#)
+
+rm -Rf sslcerts
+rm producer.config
+#rm -R cfk-dir
+#rm -R confluent-for-kubernetes
